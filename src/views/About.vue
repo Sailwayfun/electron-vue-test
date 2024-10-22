69acb1393383a45dce7b226e3c33f61dbb93998f
Sailwayfun <liaoleon000513@gmail.com>
2024-10-22 21:44:06 +0800

    set up router

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <router-view />
-  </div>
+  <div>About</div>
+  <router-link to="/">Home</router-link>
 </template>
 
 <script setup></script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Home</div>
+  <div class="nav">
+    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.nav {
+  display: flex;
+  gap: 10px;
+}
+</style>
