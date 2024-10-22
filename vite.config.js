11c7238ec7f30d1c51cef8327308f347b4a681b4
Sailwayfun <liaoleon000513@gmail.com>
2024-10-23 06:30:27 +0800

    load env

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
 
-export default defineConfig({
-    base: './',  // 確保相對路徑正確
-    plugins: [vue()],
-    build: {
-        outDir: 'dist/CIB',
-    },
-    resolve: {
-        alias: {
-            "@": "/src",
+export default defineConfig(({ mode }) => {
+    const projectName = 'CIB';
+    const envDir = path.resolve(__dirname, `envs/${projectName}`);
+    const env = loadEnv(mode, envDir);
+    return {
+        base: env.VITE_BASE_URL || './',  // 確保相對路徑正確
+        plugins: [vue()],
+        build: {
+            outDir: 'dist/CIB',
         },
-    },
+        resolve: {
+            alias: {
+                "@": "/src",
+            },
+        },
+    };
 });
